f80937e024042dfd232088ff626c9c2fc70e08ac
Nikhil Jogesh <nikhiljogesh2007@gmail.com>
2022-02-17 13:11:02 +0530

    Small grammatical changes to the about me section of the home page

--- website/views.py
+++ website/views.py
@@ -1,18 +1,21 @@
 from flask import Blueprint, render_template
+
 # This file is used to store all the URLs the user can view
 
 views = Blueprint("views", __name__)
+
 
 @views.route("/home")
 @views.route("/")
 def home():
     return render_template("home.html")
 
+
 @views.route("/projects")
 def projects():
     return render_template("projects.html")
 
+
 @views.route("/contacts")
 def contacts():
     return render_template("contacts.html")
-
