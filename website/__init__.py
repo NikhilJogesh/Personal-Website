ee7f319ba5280f321817892e3e9d6dd0906d6282
Nikhil Jogesh <nikhiljogesh2007@gmail.com>
2022-02-17 13:56:25 +0530

    Isolation of production(secret) keys

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,34 @@
 from flask import Flask
 
+# Context Manger
+class SecretKeyFile:
+    def __init__(self, filename, method):
+        try:
+            self.file = open(filename, method)
+        # Creates a new file and write a defual value to it
+        except FileNotFoundError:
+            self.file = open("secret_key.txt", "w+")
+            self.file.write("default")
+
+    def __enter__(self):
+        return self.file
+
+    def __exit__(self, type, value, traceback):
+        self.file.close()
 
 def create_app():
+
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "secure_web"
+
+    with SecretKeyFile("secret_key.txt", "r") as f:
+        secret_key = f.readline()
+        app.config['SECRET_KEY'] = secret_key
+
 
     from .views import views
 
     app.register_blueprint(views, url_prefix="/")
+
+
 
     return app
