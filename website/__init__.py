a16748562d347f786f0b07c00ebf21b97e6e7668
Nikhil Jogesh <nikhiljogesh2007@gmail.com>
2022-08-29 18:16:41 +0530

    Fixed- Blog database issue\

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+db = SQLAlchemy()
+
+DB_DIR = "dbdir"
+DB_FILE_NAME = "database.db"
+DB_PATH = f"{os.getcwd()}\{DB_DIR}\{DB_FILE_NAME}"
 
 # Context Manger
 class SecretKeyFile:
@@ -20,15 +29,32 @@ def create_app():
 
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_PATH}'
+    db.init_app(app)
+
     with SecretKeyFile("secret_key.txt", "r") as f:
         secret_key = f.readline()
         app.config['SECRET_KEY'] = secret_key
 
 
     from .views import views
+    from .blog import blog
+    from.admin import admin
 
     app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(blog, url_prefix="/blog")
+    app.register_blueprint(admin, url_prefix="/admin")
 
+    from .models import User, Post
 
+    create_database(app)
 
     return app
+
+def create_database(app):
+    #file_path = path.abspath(getcwd())+"database/database.db"
+
+    if not os.path.exists(DB_PATH):
+        os.makedirs("dbdir")
+        db.create_all(app=app)
+        print("Created Database!")
